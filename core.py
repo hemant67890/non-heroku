df4f69b6c3d475cbd6ce40913e14ee789028fac6
hemant67890 <godarasubhash337@gmail.com>
2025-03-06 20:03:17 +0530

    Update core.py

--- core.py
+++ core.py
@@ -376,7 +376,7 @@ def split_video(filename, max_size):
 
 #-----------------------Emoji handler------------------------------------
 
-EMOJIS = ["🦁", "🐶", "🐼", "🐱", "👻", "🐻‍❄️", "☁️", "🌧️", "🦁", "🐠", "🦋"]
+EMOJIS = ["🦁", "🐼"]
 emoji_counter = 0  # Initialize a global counter
 
 def get_next_emoji():
